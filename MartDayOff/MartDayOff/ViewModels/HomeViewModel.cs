ca9fca83a683e0a3ce61594bd5fd88c2d2d5921a
구본철 <bbon@bbon.kr>
2019-12-08 22:37:42 +0900

    첫 페이지 추가
    - 휴점, 개점을 표시합니다
    - 오늘 날짜와 요일을 표시합니다.
    - 배경색으로 휴점, 개점을 표시합니다

--- MartDayOff/MartDayOff/ViewModels/HomeViewModel.cs
+++ MartDayOff/MartDayOff/ViewModels/HomeViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using kr.bbon.Xamarin.Forms;
 using Xamarin.Forms;
 
@@ -10,15 +11,20 @@ namespace MartDayOff.ViewModels
         public HomeViewModel()
             : base()
         {
-
-        }
-
-        
+            Title = "마트쉬는 날";
+            initialize();
+        }        
 
         public string BackgoundColor
         {
             get => backgroundColor;
             set => SetProperty(ref backgroundColor, value);
+        }
+
+        public string TextColor
+        {
+            get => textColor;
+            set => SetProperty(ref textColor, value);
         }
 
         public string StatusText
@@ -27,23 +33,47 @@ namespace MartDayOff.ViewModels
             set => SetProperty(ref statusText, value);
         }
 
-        private void CheckDayOff()
-        {
-            if (DateTime.Now.DayOfWeek == DayOfWeek.Sunday)
-            {
-                DateTime.Now
-            }
+        public string TodayText {
+            get => todayText;
+            set => SetProperty(ref todayText, value);
         }
 
-        private void Get2ndAnd4thSunday(DateTime basis)
+        public string Description
         {
-            var results = new List<DateTime>();
+            get => description;
+            set => SetProperty(ref description, value);
+        }
+
+        private void initialize()
+        {
+            var dayoff = CheckDayOff();
+
+            CultureInfo cultures = CultureInfo.CreateSpecificCulture("ko-KR");
+            TodayText =string.Format(cultures, "오늘은 {0:yyyy년 MM월 dd일 ddd요일}입니다.", DateTime.Now);
+
+            BackgoundColor = dayoff ? "#aadd0000" : "#aa00dd00";
+            TextColor = dayoff ? "#eedddddd" : "#eedddddd";
+            StatusText = dayoff ? "휴점" : "개점";
+
+            Description = "대형 마트는 월 2회 의무 휴업이 강제되고 있습니다. 보통 두번째 일요일, 네번째 일요일을 휴무일로 정해져 있습니다.";
+        }
+
+        private bool CheckDayOff()
+        {
+            var today = DateTime.Now.AddDays(1);
+            var list = Get2ndAnd4thSunday(today);
+            return list.Contains($"{today:yyyy-MM-dd}");
+        }
+
+        private List<string> Get2ndAnd4thSunday(DateTime basis)
+        {
+            var results = new List<string>();
             var firstDateOfMonth = new DateTime(basis.Year, basis.Month, 1);
-            var days =DateTime.DaysInMonth(basis.Year, basis.Month);
+            var days = DateTime.DaysInMonth(basis.Year, basis.Month);
 
             var check = 0;
 
-            for(int i = 0; i<days; i++)
+            for (int i = 0; i < days; i++)
             {
                 var currentDate = firstDateOfMonth.AddDays(i);
                 if (currentDate.DayOfWeek == DayOfWeek.Sunday)
@@ -51,14 +81,18 @@ namespace MartDayOff.ViewModels
                     check += 1;
                 }
 
-                if(check == 2 || check == 4) {
-                    results.Add(currentDate);
+                if (check == 2 || check == 4) {
+                    results.Add($"{ currentDate:yyyy-MM-dd}");
                 }
             }
+            return results;
         }
 
         private string backgroundColor;
+        private string textColor;
+        private string description;
         private string statusText;
+        private string todayText;
     }
 }
 
